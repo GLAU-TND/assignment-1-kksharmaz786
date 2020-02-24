d3b1844f2ad3c90f624116c18ce3c8bdd0849fe7
krishna sharma <krishna.sharma_cs18@gla.ac.in>
2020-02-24 14:19:37 +0530

    getter and setter method to access and set value of variable in person class

--- src/Helper_Methods/Person.java
+++ src/Helper_Methods/Person.java
@@ -7,6 +7,51 @@ public class Person implements Comparable<Person> {
     private String emailID;                     //EmailId of person
     private StringBuffer phoneNumbers;
 
+    //Getter for first name
+    public String getFirstName() {
+        return firstName;
+    }
+
+    //Setter for first name
+    public void setFirstName(String firstName) {
+        System.out.println(firstName);
+        this.firstName = firstName;
+    }
+
+    //Getter for last name
+    public String getLastName() {
+        return lastName;
+    }
+
+    //Setter for last name
+    public void setLastName(String lastName) {
+        System.out.println(lastName);
+        this.lastName = lastName;
+    }
+
+    //Getter for Email ID
+    public String getEmailID() {
+        return emailID;
+    }
+
+    //Setter for EmailID
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    //Getter for fetching all phone numbers
+    public StringBuffer getPhoneNumber() {
+        return this.phoneNumbers;
+    }
+
+    //Setter for inserting new phone numbers
+    public void setPhoneNumbers(String phoneNumber) {
+        if (phoneNumbers == null) {
+            this.phoneNumbers = new StringBuffer(phoneNumber);
+        } else {
+            this.phoneNumbers.append(", " + phoneNumber);
+        }
+    }
 
     @Override
     public int compareTo(Person person) {
