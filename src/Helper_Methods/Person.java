d4d7462662f06e39f525c3805a843abbff1ec5b7
krishna sharma <krishna.sharma_cs18@gla.ac.in>
2020-02-24 14:18:57 +0530

    variable to store details of individual person

--- src/Helper_Methods/Person.java
+++ src/Helper_Methods/Person.java
@@ -2,6 +2,12 @@ package Helper_Methods;
 
 public class Person implements Comparable<Person> {
 
+    private String firstName;                   //First name of person
+    private String lastName;                    //Last name of person
+    private String emailID;                     //EmailId of person
+    private StringBuffer phoneNumbers;
+
+
     @Override
     public int compareTo(Person person) {
         return 0;
